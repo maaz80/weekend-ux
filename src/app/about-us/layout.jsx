79fed5680ac0f93f650e68ea05f9417f22975f5b
maaz80 <maazforlap@gmail.com>
2026-08-04 13:58:54 +0530

    added deploy.yml

--- src/app/about-us/layout.jsx
+++ src/app/about-us/layout.jsx
@@ -4,6 +4,7 @@ export async function generateMetadata() {
      return generatePageMetadata("about-us", "About Us - Weekend UX", "Learn more about Weekend UX, our team, mission, and philosophy behind designing premium learning experiences.", "/about-us");
 }
 
+
 export default function AboutLayout({ children }) {
      return <>{children}</>;
 }
